1fc6e9108bad01ac6a6c8ab6024def63d3a7ef28
Erfan Tavana <84908961+ErfanTavana@users.noreply.github.com>
2024-06-24 09:01:12 +0330

    The message has been received

--- Chat/urls.py
+++ Chat/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import index, chat_details, file_upload_view
+from .views import index, chat_details, file_upload_view, get_messages
 
 # urls.py
 
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', index, name='index_name'),
     path('chat_details/<int:id>', chat_details, name='chat_details_name'),
     path('upload/', file_upload_view, name='file_upload'),
+    path('chat/<int:chat_id>/messages/', get_messages, name='get_messages'),
 ]
--- Chat/views.py
+++ Chat/views.py
@@ -52,3 +52,30 @@ def file_upload_view(request):
     else:
         form = FileUploadForm()
     return render(request, 'file_upload.html', {'form': form})
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def get_messages(request, chat_id):
+    chat_detail = get_object_or_404(Chat, id=chat_id)
+
+    # Check if the user is a participant in the chat
+    if request.user not in [chat_detail.participant1, chat_detail.participant2]:
+        return HttpResponseForbidden("You do not have access to view this chat.")
+
+    messages = Message.objects.filter(chat=chat_detail).values('id', 'sender__username', 'content', 'timestamp', 'file')
+    messages_list = list(messages)
+
+    for message in messages_list:
+        message['file_url'] = ''
+        message['file_name'] = ''
+        message['file_size'] = ''
+        if message['file']:
+            message_instance = Message.objects.get(id=message['id'])
+            if message_instance.file:
+                message['file_url'] = message_instance.file.url
+                message['file_name'] = message_instance.file.name
+                message['file_size'] = message_instance.file.size
+
+    return JsonResponse(messages_list, safe=False)
