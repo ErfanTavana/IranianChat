922975d2cb9cbdd640e791e1942cf780a1e3b4e7
Erfan Tavana <84908961+ErfanTavana@users.noreply.github.com>
2024-06-29 12:55:45 +0330

    111

--- Account/urls.py
+++ Account/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
 
-from .views import login, logout_view
-
-# urls.py
+from .views import login, logout_view, register
 
 urlpatterns = [
+    path('register/', register, name='register_name'),
     path('login/', login, name='login_name'),
     path('logout/', logout_view, name='logout_name'),
-
 ]
--- Account/views.py
+++ Account/views.py
@@ -1,7 +1,51 @@
-from django.shortcuts import render
-from django.shortcuts import render, redirect
+import random
+import string
+
+import ghasedakpack
 from django.contrib.auth import authenticate, login as auth_login
 from django.contrib.auth import logout
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+
+from .models import Profile
+
+
+def generate_random_password():
+    return ''.join(random.choices(string.digits, k=6))
+
+
+def register(request):
+    if request.user.is_superuser:
+        if request.method == 'GET':
+            return render(request, 'register.html')
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            first_name = request.POST.get('first_name')
+            last_name = request.POST.get('last_name')
+            profile_picture = request.FILES.get('profile_picture')  # اصلاح به Files
+            responsibility = request.POST.get('responsibility')
+            phone_number = username
+            try:
+                user = User.objects.get(username=username)
+                return render(request, 'register.html', {'error': 'نام کاربری از قبل وجود دارد'})
+            except User.DoesNotExist:
+                password_random = generate_random_password()
+                user = User.objects.create_user(username=username, password=password_random)
+                Profile.objects.create(user_id=user.id, first_name=first_name, last_name=last_name,
+                                       profile_picture=profile_picture, responsibility=responsibility,
+                                       phone_number=phone_number)
+                print(password_random)
+                # ارسال کد تایید از طریق سرویس پیامکی
+                sms = ghasedakpack.Ghasedak("1feaff6b0fb9ab14d5f1b9acc9fcad839699b313816e3001e128cef8e6271850")
+                print(sms.verification({'receptor': f'{phone_number}', 'type': '1', 'template': 'NewEmployeePassword',
+                                        'param1': f'{phone_number}', 'param2': f'{password_random}'}))
+                return render(request, 'register.html', {'ok': 'کارمند با موفقیت اضافه شد'})
+    else:
+        if request.user.is_authenticated:
+            return redirect('index_name')
+        else:
+            return redirect('login_name')
+
 
 def login(request):
     if request.method == 'GET':
@@ -22,6 +66,7 @@ def login(request):
                 'username': username,
             })
     return render(request, 'login.html')
+
 
 def logout_view(request):
     logout(request)
