8a1a00448e196d7bb9e9d32689bc10780cff6e8c
Erfan Tavana <84908961+ErfanTavana@users.noreply.github.com>
2024-07-01 10:02:42 +0330

    Change time zone

--- IranianChat/settings.py
+++ IranianChat/settings.py
@@ -113,14 +113,16 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
+LANGUAGE_CODE = 'fa-ir'  # زبان فارسی
 
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tehran'  # منطقه زمانی ایران
 
 USE_I18N = True
 
+USE_L10N = True
+
 USE_TZ = True
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
